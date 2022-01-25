c8e6cd0f430621d0aa8b6da3abc86db7709dd5c9
Fabian (iPad) <sfabianlyl@gmail.com>
2022-01-25 15:48:54 +0800

    email

--- config/mail.php
+++ config/mail.php
@@ -43,6 +43,13 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
+            'stream' => [
+                'ssl' => [
+                    'allow_self_signed' => true,
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                ],
+            ],
         ],
 
         'ses' => [
@@ -107,12 +114,6 @@ return [
         ],
     ],
     
-    'stream' => [
-        'ssl' => [
-            'allow_self_signed' => true,
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-        ],
-    ],
+    
 
 ];
