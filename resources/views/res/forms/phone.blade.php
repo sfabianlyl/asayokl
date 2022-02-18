ee48aee93ff4df83470f2c13e2ebc4e966e3a4e0
Fabian (iPad) <sfabianlyl@gmail.com>
2022-02-18 16:34:04 +0800

    phone

---
+++ resources/views/res/forms/phone.blade.php
@@ -0,0 +1,9 @@
+<div class="row mb-5 align-items-center">
+    <div class="col-4">No. Telefon (Phone Number)</div>
+    <div class="col-8">
+        <div class="d-flex align-items-center">
+            <span class="px-3">+6</span>
+            <div style="flex:1;"><input type="text" name="phone" class="form-control" value="" required></div>
+        </div>
+    </div>
+</div>
