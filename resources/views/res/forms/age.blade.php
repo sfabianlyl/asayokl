c78df0787968ab9e2707dc65c6c57479d1a5527c
Fabian <sfabianlyl@gmail.com>
2022-03-25 14:19:34 +0800

    changes kamikudus form

--- resources/views/res/forms/age.blade.php
+++ resources/views/res/forms/age.blade.php
@@ -4,7 +4,7 @@
         <label><input type="radio" name="age" value="18 - 21" checked>18 - 21 tahun (18 - 21 years old)</label><br>
         <label><input type="radio" name="age" value="22 - 24">22 - 24 tahun (22 - 24 years old)</label><br>
         <label><input type="radio" name="age" value="25 - 29">25 - 29 tahun (25 - 29 years old)</label><br>
-        <label><input type="radio" name="age" value="30 - 34">30 - 34 tahun (30 - 34 years old)</label>
+        <label><input type="radio" name="age" value="30 - 34">30 - 34 tahun (30 - 34 years old)</label><br>
         <label><input type="radio" name="age" value="35 - 39">35 - 39 tahun (35 - 39 years old)</label>
     </div>
 </div>
