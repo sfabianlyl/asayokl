52b44f88e35d8f5cc87a4298b5ec0a911335ae65
Fabian <sfabianlyl@gmail.com>
2022-05-06 15:49:54 +0800

    team changes

--- resources/views/contents/team.blade.php
+++ resources/views/contents/team.blade.php
@@ -19,7 +19,7 @@
         </div>
     </div>
 
-    <!-- Director -->
+    {{-- Director --}}
     <div class="row justify-content-center pad-btm-50">
         <div class="col-lg-8 col-12 post-title text-center pad-btm-15">
             <h4 class="bm" style="display:none">Pengarah / Penolong Gerejawi</h4>
@@ -33,7 +33,7 @@
         </div>
     </div>
 
-    <!-- Assistant Director -->
+    {{-- Assistant Director --}}
     <div class="row justify-content-center pad-btm-50">
         <div class="col-lg-8 col-12 post-title text-center pad-btm-15">
             <h4 class="bm" style="display:none">Penolong Pengarah / Penolong Gerejawi</h4>
@@ -47,22 +47,22 @@
         </div>
     </div>
 
-    <!-- Secretary -->
-    {{-- <div class="row justify-content-center pad-btm-50">
+    {{-- Secretary --}}
+    <div class="row justify-content-center pad-btm-50">
         <div class="col-lg-8 col-12 post-title text-center pad-btm-15">
             <h4 class="bm" style="display:none">Setiausaha</h4>
             <h4 class="eng" style="display:none">Secretary</h4>
         </div>
         <div class="col-lg-8 col-12 text-center">
-            <h5>Ms Christina Chew</h5>
+            <h5>Ms Brenda Ng</h5>
         </div>
         <div class="col-lg-8 col-12 text-center">
             <a href="mailto:asayo@archkl.org"><h6><i>asayo@archkl.org</i></h6></a>
         </div>
-    </div> --}}
+    </div>
 
 
-    <!-- Pastoral Workers -->
+    {{-- Pastoral Workers --}}
 
     <div class="row justify-content-center pad-btm-30">
         <div class="col-lg-8 col-12 post-title text-center">
@@ -87,6 +87,14 @@
                     </div>
                     <div class="col-lg-12 col-12 text-center">
                         <a href="mailto:agnes@asayokl.my"><h6><i>agnes@asayokl.my</i></h6></a>
+                    </div>
+                </div>
+                <div class="col-lg-6 text-center pad-btm-15">
+                    <div class="col-lg-12 col-12 text-center">
+                        <h5>Edmund Raymond</h5>
+                    </div>
+                    <div class="col-lg-12 col-12 text-center">
+                        <a href="mailto:edmundray@asayokl.my"><h6><i>edmundray@asayokl.my</i></h6></a>
                     </div>
                 </div>
                 <div class="col-lg-6 text-center pad-btm-15">
