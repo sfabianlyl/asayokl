6c90fe7d1312e84958bcb6f57f24a40e4a753a58
sfabianlyl <sfabianlyl@gmail.com>
2023-07-24 11:10:17 +0800

    ig feed

--- resources/views/contents/home.blade.php
+++ resources/views/contents/home.blade.php
@@ -73,7 +73,7 @@
 
     <div>
         <h4 class="text-center mb-5">ArchKL Pilgrims' Updates from World Youth Day 2023</h4>
-        <div id="instagramContainer" class="d-flex justify-content-around flex-wrap mb-5"></div>
+        <div id="instagramContainer" hashtag="#wydarchkl" class="d-flex justify-content-around flex-wrap mb-5"></div>
         <div class="text-center">
             <a href="https://www.instagram.com/asayo_kl/" class="btn btn-primary" target="_blank">See More</a>
         </div>
