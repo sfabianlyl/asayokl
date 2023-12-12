d0cb8fc2f8bb19a80d789b4e14d240887562538f
sfabianlyl <sfabianlyl@gmail.com>
2023-12-12 11:32:25 +0800

    diet view

--- resources/views/res/forms/diet.blade.php
+++ resources/views/res/forms/diet.blade.php
@@ -1,11 +1,8 @@
 <div class="row mb-5 align-items-center">
-    <div class="col-12">
-        <div class="mb-3">
-            Keperluan Diet <br><i>(Dietary requirements)</i> 
-        </div>
-        <div class="mb-3">
-            <label><input type="radio" name="diet" value="None" required>Tiada (none)</label><br>
-            <label><input type="radio" name="diet" value="Vegetarian" required>Vegetarian</label>
-        </div>
+    <div class="col-4">Keperluan Diet <br><i>(Dietary requirements)</i></div>
+    <div class="col-8">
+        <label><input type="radio" name="diet" value="None" required>Tiada (none)</label><br>
+        <label><input type="radio" name="diet" value="Vegetarian" required>Vegetarian</label>
     </div>
+    
 </div>
