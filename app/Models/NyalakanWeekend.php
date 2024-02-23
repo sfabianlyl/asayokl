d45122d5d5e8ec7fd026d4dfdcefd0230ff08000
Fabian Lee <sfabianlyl@gmail.com>
2024-02-23 13:42:04 +0800

    nyalakan weekend

---
+++ app/Models/NyalakanWeekend.php
@@ -0,0 +1,17 @@
+<?php
+    namespace App\Models;
+
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\SoftDeletes;
+    class NyalakanWeekend extends Model
+    {
+        use SoftDeletes;
+        protected $fillable = [
+            'name',
+        ];
+
+        public function participants()
+        {
+            return $this->hasMany(NyalakanParticipant::class);
+        }
+    }
